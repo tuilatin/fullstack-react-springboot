64769f2f1e637b1eb59f84479aee170c1c4ad48f
tuilatin <nguyentin1763@gmail.com>
2026-08-19 15:20:50 +0700

    18/3

--- section3/eazystore-ui/src/App.jsx
+++ section3/eazystore-ui/src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header";
 import Footer from "./components/footer/Footer";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   );
