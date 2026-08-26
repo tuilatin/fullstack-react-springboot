016c4da6c7b6e668c4a68b9cec60224ba99b6ab3
tuilatin <nguyentin1763@gmail.com>
2026-08-26 09:11:44 +0700

    localStorage.setItem(theme, newTheme)

--- section9/eazystore-ui/src/components/Header.jsx
+++ section9/eazystore-ui/src/components/Header.jsx
@@ -18,6 +18,7 @@ export default function Header() {
       } else {
         document.documentElement.classList.remove("dark");
       }
+      localStorage.setItem("theme", newTheme);
       return newTheme;
     });
   };
