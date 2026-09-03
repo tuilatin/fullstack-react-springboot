b7887cb6822567e22ea1d88d653c89832b592944
tuilatin <nguyentin1763@gmail.com>
2026-09-03 15:01:24 +0700

    16/11 7:25

--- section11/eazystore/src/main/java/com/eazybytes/eazystore/dto/ContactRequestDto.java
+++ section11/eazystore/src/main/java/com/eazybytes/eazystore/dto/ContactRequestDto.java
@@ -1,5 +1,9 @@
 package com.eazybytes.eazystore.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,9 +14,20 @@ import java.time.Instant;
 @Setter
 public class ContactRequestDto {
 
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min =5, max = 30, message = "Name must be between 5 and 30 characters")
     private String name;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email address")
     private String email;
+
+    @NotBlank(message = "Mobile Number cannot be empty")
+    @Pattern(regexp = "^\\d{10}$", message = "Mobile number must be 10 digits")
     private String mobileNumber;
+
+    @NotBlank(message = "Message cannot be empty")
+    @Size(min =5, max = 500, message = "Message must be between 5 and 500 characters")
     private String message;
 
 }
