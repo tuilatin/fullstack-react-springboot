543f59451991c53263a9ab0816d3ab39e270fefc
tuilatin <nguyentin1763@gmail.com>
2026-09-09 10:24:15 +0700

    7/13 1:40

--- section11/eazystore/src/main/java/com/eazybytes/eazystore/security/EazystoreSecurityConfig.java
+++ section11/eazystore/src/main/java/com/eazybytes/eazystore/security/EazystoreSecurityConfig.java
@@ -8,7 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -23,10 +29,25 @@ public class EazystoreSecurityConfig {
     @Bean
      SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        return http.authorizeHttpRequests((requests) ->{
-            
+        return http.csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests((requests) ->{
+            publicPaths.forEach(path -> requests.requestMatchers(path).permitAll());
+            requests.anyRequest().authenticated();
                 })
                         .formLogin(withDefaults())
                         .httpBasic(withDefaults()).build();
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
